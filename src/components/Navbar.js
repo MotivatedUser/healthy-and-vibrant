e4b858701cb547912fd93e4a838383d0291cb2f1
MotivatedUser <carbfixer@gmail.com>
2023-04-27 12:48:07 -0700

    Adds more responsiveness to Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,54 +22,57 @@ const Header = () => {
 
   return (
     <>
-      <Navbar dark color="primary" sticky="top" expand="md">
+      <Navbar dark expand="md" className="custom-navbar">
         <div className="navbar-content">
-          
-            <NavbarBrand className="ms-5 navbar-left" href="/">
-              <img
-                src={CitrusPic}
-                alt="Colorful fruits slices"
-                className="header-pic"
-              />
-            </NavbarBrand>
-          
+          <NavbarBrand className="ms-5 navbar-left" href="/">
+            <img
+              src={CitrusPic}
+              alt="Colorful fruits slices"
+              className="header-pic"
+            />
+          </NavbarBrand>
+
           <div className="navbar-middle">
             <h1 className="mt-1">Healthy and Vibrant</h1>
             <p>Providing wellness through nutrition and lifestyle</p>
           </div>
-          
-            <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
-            <Collapse isOpen={menuOpen} navbar>
-              <Nav className="ms-auto navbar-right" navbar>
-                <NavItem>
-                  <NavLink className="nav-link" to="/" href="/">
-                    <FcHome /> Home
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink className="nav-link" to="/about" href="/about">
-                    <FcAbout /> About
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink className="nav-link" to="/schedule" href="/projects">
-                    <FcBinoculars /> Schedule
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink className="nav-link" to="/order-here" href="/resume">
-                    <FcBusinessman /> Order
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink className="nav-link" to="/blog" href="/skills">
-                    <FcAssistant /> Blog
-                  </NavLink>
-                </NavItem>
-              </Nav>
-            </Collapse>
-          </div>
-        
+
+          <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
+          <Collapse isOpen={menuOpen} navbar className="custom-navbar-collapse">
+            <Nav
+              className={`ms-auto navbar-right${
+                menuOpen ? " expanded-nav-items" : ""
+              }`}
+              navbar
+            >
+              <NavItem>
+                <NavLink className="nav-link" to="/" href="/">
+                  <FcHome /> Home
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to="/about" href="/about">
+                  <FcAbout /> About
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to="/schedule" href="/projects">
+                  <FcBinoculars /> Schedule
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to="/order-here" href="/resume">
+                  <FcBusinessman /> Order
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to="/blog" href="/skills">
+                  <FcAssistant /> Blog
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </div>
       </Navbar>
     </>
   );
---
+++ src/pages/OrderHere.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OrderHere = () => {
+  return (
+    <div>OrderHere</div>
+  )
+}
+
+export default OrderHere
